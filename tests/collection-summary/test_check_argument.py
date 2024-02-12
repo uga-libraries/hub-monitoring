ea7e1e6b26b33bf27ad404e1c9242306d52e73ac
Adriane Hanson <ahansonuga@gmail.com>
2024-02-12 09:01:54 -0500

    Simplify test_check_argument
    Don't need the path to the script to be accurate, and function is already returning a tuple to use for result.

--- tests/collection-summary/test_check_argument.py
+++ tests/collection-summary/test_check_argument.py
@@ -1,32 +1,29 @@
 """
 Tests for the function check_argument(), which verifies the required argument is present and a valid path.
+In production, the input is from sys.argv
 """
-import os
 import unittest
 from collection_summary import check_argument
+from os import getcwd
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_correct(self):
         """Test for when the directory argument is present and a valid path."""
-        directory, error = check_argument([os.path.join(os.getcwd(),'..', 'collection_summary.py'),
-                                           os.getcwd()])
-        result = (directory, error)
-        expected = (os.getcwd(), None)
+        result = check_argument(['collection_summary.py', getcwd()])
+        expected = (getcwd(), None)
         self.assertEqual(result, expected, "Problem with test for correct directory argument")
 
     def test_directory_missing(self):
         """Test for when the directory argument is not present."""
-        directory, error = check_argument([os.path.join(os.getcwd(),'..', 'collection_summary.py')])
-        result = (directory, error)
+        result = check_argument(['collection_summary.py'])
         expected = (None, "Missing required argument: directory")
         self.assertEqual(result, expected, "Problem with test for directory argument missing")
 
     def test_directory_invalid(self):
         """Test for when the directory argument is not a valid path."""
-        directory, error = check_argument([os.path.join(os.getcwd(),'..', 'collection_summary.py'), "path/error"])
-        result = (directory, error)
+        result = check_argument(['collection_summary.py', 'path/error'])
         expected = (None, "Provided directory path/error does not exist")
         self.assertEqual(result, expected, "Problem with test for correct directory argument")
 
