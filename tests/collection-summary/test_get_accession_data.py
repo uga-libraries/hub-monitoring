3d8ea0ffacacfea58517a525718c0b4adf58c492
Adriane Hanson <ahansonuga@gmail.com>
2024-02-16 10:38:21 -0500

    Update test result: size and files is for data folder

--- tests/collection-summary/test_get_accession_data.py
+++ tests/collection-summary/test_get_accession_data.py
@@ -12,7 +12,7 @@ class MyTestCase(unittest.TestCase):
     def test_function(self):
         directory = join(getcwd(), '..', 'test_data', 'Russell_Hub')
         accession_list = get_accession_data(directory, 'backlog', 'rbrl002', '2022-01-er')
-        accession_list_expected = ['rbrl002', 'backlog', '2022', .000000211, 4, 2, 3, 4, 5]
+        accession_list_expected = ['rbrl002', 'backlog', '2022', .000000018, 2, 2, 3, 4, 5]
         self.assertEqual(accession_list, accession_list_expected, "Problem with test for the function")
 
 
