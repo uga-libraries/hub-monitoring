412d5660153d43d7ed49404f068048cac6646664
Adriane Hanson <ahansonuga@gmail.com>
2024-04-01 12:47:14 -0400

    Reorder combine_collection_data()
    Comments make more sense if the dropping of the Date column is grouped with the other date code.

--- collection_summary.py
+++ collection_summary.py
@@ -43,14 +43,14 @@ def combine_collection_data(acc_df):
 
     # Replaces risk columns that have file counts with risk columns that have the percentage of files.
     # If files have multiple identifications, the combined percentages will be more than 100%.
-    # Also removes the Date column, so it doesn't conflict with the column from combine_collection_dates().
     coll_df['No_Match_Risk_%'] = (coll_df['No_Match_Risk'] / coll_df['Files'] * 100).map(round_non_zero)
     coll_df['High_Risk_%'] = (coll_df['High_Risk'] / coll_df['Files'] * 100).map(round_non_zero)
     coll_df['Moderate_Risk_%'] = (coll_df['Moderate_Risk'] / coll_df['Files'] * 100).map(round_non_zero)
     coll_df['Low_Risk_%'] = (coll_df['Low_Risk'] / coll_df['Files'] * 100).map(round_non_zero)
-    coll_df.drop(['Date', 'No_Match_Risk', 'High_Risk', 'Moderate_Risk', 'Low_Risk'], axis=1, inplace=True)
 
     # Combines the dates into a date range and adds to the dataframe.
+    # Removes the existing "Date" column, so it doesn't conflict with the new "Date" column made by the function.
+    coll_df.drop(['Date', 'No_Match_Risk', 'High_Risk', 'Moderate_Risk', 'Low_Risk'], axis=1, inplace=True)
     date_df = combine_collection_dates(acc_df)
     coll_df = pd.merge(date_df, coll_df, on='Collection', how='outer')
 
