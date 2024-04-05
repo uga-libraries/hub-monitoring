43ca7218d44fa57a1a57e8e0e0a1e28217946adf
Adriane Hanson <ahansonuga@gmail.com>
2024-04-05 15:35:06 -0400

    Update comments

--- format_list.py
+++ format_list.py
@@ -24,7 +24,7 @@ def combine_risk_csvs(dir_path):
     df (pandas DataFrame): dataframe with all columns from every risk csv
     """
 
-    # Makes a list of every risk spreadsheet, anywhere in the directory.
+    # Makes a list of every accession risk spreadsheet, anywhere in the directory.
     csv_list = []
     for root, directories, files in os.walk(dir_path):
         for file in files:
@@ -34,42 +34,45 @@ def combine_risk_csvs(dir_path):
     # Combines every spreadsheet into one dataframe.
     df = pd.concat([pd.read_csv(f) for f in csv_list])
 
-    # Converts version to string. If a CSV has all numeric versions, it is a float, but otherwise a string.
-    # The data type needs to be the same for combining different instances of the same version.
+    # Converts the version column to a string.
+    # By default, if a CSV has all numeric versions, it is a float, but otherwise a string.
+    # The data type needs to be the same for combining different instances of the same format version later.
     df['FITS_Format_Version'] = df['FITS_Format_Version'].astype(str)
 
     return df
 
 
 def df_cleanup(df):
-    """Remove columns, remove duplicates, fill empty NARA risk levels
+    """Remove unneeded columns, rename a column, remove duplicates, fill empty NARA risk levels
 
     @:parameter
-    df (pandas DataFrame): dataframe with all columns from every csv
+    df (pandas DataFrame): dataframe with all columns from every risk csv
 
     @:returns
-    df (pandas DataFrame): dataframe with select columns from every csv
+    df (pandas DataFrame): dataframe with select columns from every risk csv
     """
 
-    # Makes a copy of the dataframe with just the needed columns.
+    # Keeps only the needed columns.
+    # The other FITs data, NARA data, technical appraisal, and other risk columns are not used.
     df = df[['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB', 'NARA_Risk Level']]
 
-    # Rename the only column with spaces to use underscores instead.
+    # Renames the only column with spaces to use underscores instead.
     df = df.rename({'NARA_Risk Level': 'NARA_Risk_Level'}, axis=1)
 
-    # Removes duplicates from multiple NARA matches to the same file with the same NARA risk level.
-    # The file path column is only needed for removing duplicates.
+    # Removes duplicates from multiple NARA matches with the same NARA risk level to the same file.
+    # The file will still be repeated once per NARA risk level.
+    # The file path column is then removed, since it is only needed for removing duplicates.
     df = df.drop_duplicates()
     df = df.drop(columns='FITS_File_Path')
 
-    # Fill blanks in NARA (legacy practice) with No Match (current practice).
+    # Fill blanks in the NARA risk level column (legacy practice) with "No Match" (current practice).
     df['NARA_Risk_Level'] = df['NARA_Risk_Level'].fillna('No Match')
 
     return df
 
 
 def files_per_format(df):
-    """Number of files for each format name, version, risk level combination
+    """Calculate the number of files for each format name, version, and NARA risk level combination
 
     @:parameter
     df (pandas DataFrame): dataframe with select columns form every csv (output of df_cleanup())
@@ -78,20 +81,20 @@ def files_per_format(df):
     files (pandas DataFrame): dataframe with format name, version, NARA risk level, and number of files
     """
 
-    # Groupby includes NARA risk level so different possible risks for the name/version combination are kept.
+    # Groupby includes NARA risk level so different possible risks for each name/version combination are kept.
     # Reset index keeps the name, version, and risk level as columns in the dataframe.
     # Without reset index, it would return a series with those columns as part of the index.
     group_list = ['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']
     files = df.groupby(group_list).size().reset_index()
 
-    # Renames the default column name for the sum to be more descriptive.
+    # Renames the default column name for the count to be more descriptive.
     files = files.rename({0: 'File_Count'}, axis=1)
 
     return files
 
 
 def size_per_format(df):
-    """Size in GB for each format name, version, risk level combination
+    """Calculate the size in GB for each format name, version, and NARA risk level combination
 
     @:parameter
     df (pandas Dataframe): dataframe with select columns form every csv (output of df_cleanup())
@@ -106,10 +109,11 @@ def size_per_format(df):
     group_list = ['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']
     size = df.groupby(group_list)['FITS_Size_KB'].sum().reset_index()
 
-    # Convert the KB to GB and round to 3 decimal places.
+    # Converts the KB to GB and rounds to 3 decimal places.
+    # Anything less than 1 MB with have a size of 0.
     size['Size_GB'] = round(size['FITS_Size_KB'] / 1000000, 3)
 
-    # Remove the KB column, which is not needed now that we have GB.
+    # Removes the KB column, which is not needed now that we have GB.
     size = size.drop(columns='FITS_Size_KB')
 
     return size
@@ -117,7 +121,7 @@ def size_per_format(df):
 
 if __name__ == '__main__':
 
-    # Gets the path to the directory with the accessions to be validated from the script argument.
+    # Gets the path to the directory with the risk CSVs to be combined from the script argument.
     # Exits the script if there is an error.
     directory, error = check_argument(sys.argv)
     if error:
@@ -127,13 +131,14 @@ if __name__ == '__main__':
     # Combines the risk data into one dataframe.
     df_all = combine_risk_csvs(directory)
 
-    # Transforms dataframe with all risk data to a dataframe with desired data.
+    # Transforms the dataframe with all risk data to a dataframe with the desired data.
     df_formats = df_cleanup(df_all)
 
-    # Gets the number of files per format and GB per format, and combines into one dataframe.
+    # Calculates the number of files per format and GB per format, and combines them into one dataframe.
+    # It also includes the format name, version, and NARA risk level.
     df_files = files_per_format(df_formats)
     df_size = size_per_format(df_formats)
     df_format_list = pd.merge(df_files, df_size, how='outer')
 
-    # Save the result to a CSV in directory.
+    # Saves the result to a CSV in the directory.
     df_format_list.to_csv(os.path.join(directory, 'combined_format_data.csv'), index=False)
