e155fefb7bf589533b2f4c9a78d9e5c74df9b141
Adriane Hanson <ahansonuga@gmail.com>
2024-04-05 10:31:19 -0400

    Clean up df in a function

--- format_list.py
+++ format_list.py
@@ -25,6 +25,27 @@ def combine_risk_csvs(dir):
 
     # Combines every spreadsheet into one dataframe.
     df = pd.concat([pd.read_csv(f) for f in csv_list])
+
+    return df
+
+
+def df_cleanup(df):
+    """Remove columns, remove duplicates, fill empty NARA risk levels"""
+
+    # Makes a copy of the dataframe with just the needed columns.
+    df = df[['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB', 'NARA_Risk Level']]
+
+    # Rename the only column with spaces to use underscores instead.
+    df = df.rename({'NARA_Risk Level': 'NARA_Risk_Level'}, axis=1)
+
+    # Removes duplicates from multiple NARA matches to the same file with the same NARA risk level.
+    # The file path column is only needed for removing duplicates.
+    df = df.drop_duplicates()
+    df = df.drop(columns='FITS_File_Path')
+
+    # Fill blanks in NARA (legacy practice) with No Match (current practice).
+    df['NARA_Risk_Level'] = df['NARA_Risk_Level'].fillna('No Match')
+
     return df
 
 
@@ -36,19 +57,13 @@ if __name__ == '__main__':
     # Combines the risk data into one dataframe.
     df_all = combine_risk_csvs(directory)
 
-    # Makes a copy of the dataframe with just the needed columns and no duplicate rows (from multiple NARA matches).
-    df = df_all[['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB', 'NARA_Risk Level']].copy()
-    df = df.rename({'NARA_Risk Level': 'NARA_Risk_Level'}, axis=1)
-    df = df.drop_duplicates()
-    df = df.drop(columns='FITS_File_Path')
-
-    # Fill blanks in NARA (legacy practice) with No Match (current practice).
-    df['NARA_Risk_Level'] = df['NARA_Risk_Level'].fillna('No Match')
+    # Transforms dataframe with all risk data to a dataframe with desired data.
+    df_formats = df_cleanup(df_all)
 
     # Number of files and KB for each format name/version/risk combination.
-    files = df.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']).size().reset_index()
+    files = df_formats.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']).size().reset_index()
     files = files.rename({0: 'File_Count'}, axis=1)
-    size = df.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level'])['FITS_Size_KB'].sum().reset_index()
+    size = df_formats.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level'])['FITS_Size_KB'].sum().reset_index()
     size['Size_GB'] = size['FITS_Size_KB']/1000000
     size = size.drop(columns='FITS_Size_KB')
     df_formats = pd.merge(files, size, how='outer')
