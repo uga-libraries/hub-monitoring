752a15e9ca224d84a98c0d274bda4a5a3b16477c
Adriane Hanson <ahansonuga@gmail.com>
2024-04-05 12:44:14 -0400

    Make quantity aggregations with functions
    Had to change the version column to string in combine_risk_csvs() or it didn't aggregate properly depending on if an accession had any non-numeric versions or not.

--- format_list.py
+++ format_list.py
@@ -13,18 +13,22 @@ import pandas as pd
 import sys
 
 
-def combine_risk_csvs(dir):
+def combine_risk_csvs(dir_path):
     """Combine the data from every risk csv in the directory into one dataframe"""
 
     # Makes a list of every risk spreadsheet, anywhere in the directory.
     csv_list = []
-    for root, directories, files in os.walk(dir):
+    for root, directories, files in os.walk(dir_path):
         for file in files:
             if 'full_risk_data' in file:
                 csv_list.append(os.path.join(root, file))
 
     # Combines every spreadsheet into one dataframe.
     df = pd.concat([pd.read_csv(f) for f in csv_list])
+
+    # Converts version to string. If a CSV has all numeric versions, it is a float, but otherwise a string.
+    # The data type needs to be the same for combining different instances of the same version.
+    df['FITS_Format_Version'] = df['FITS_Format_Version'].astype(str)
 
     return df
 
@@ -49,6 +53,39 @@ def df_cleanup(df):
     return df
 
 
+def files_per_format(df):
+    """Number of files for each format name, version, risk level combination"""
+
+    # Groupby includes NARA risk level so different possible risks for the name/version combination are kept.
+    # Reset index keeps the name, version, and risk level as columns in the dataframe.
+    # Without reset index, it would return a series with those columns as part of the index.
+    group_list = ['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']
+    files = df.groupby(group_list).size().reset_index()
+
+    # Renames the default column name for the sum to be more descriptive.
+    files = files.rename({0: 'File_Count'}, axis=1)
+
+    return files
+
+
+def size_per_format(df):
+    """Size in GB for each format name, version, risk level combination"""
+
+    # Groupby includes NARA risk level so different possible risks for the name/version combination are kept.
+    # Reset index keeps the name, version, and risk level as columns in the dataframe.
+    # Without reset index, it would return a series with those columns as part of the index.
+    group_list = ['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']
+    size = df.groupby(group_list)['FITS_Size_KB'].sum().reset_index()
+
+    # Convert the KB to GB and round to 3 decimal places.
+    size['Size_GB'] = round(size['FITS_Size_KB'] / 1000000, 3)
+
+    # Remove the KB column, which is not needed now that we have GB.
+    size = size.drop(columns='FITS_Size_KB')
+
+    return size
+
+
 if __name__ == '__main__':
 
     # Gets the path to the directory with the accessions to be validated from the script argument.
@@ -60,13 +97,10 @@ if __name__ == '__main__':
     # Transforms dataframe with all risk data to a dataframe with desired data.
     df_formats = df_cleanup(df_all)
 
-    # Number of files and KB for each format name/version/risk combination.
-    files = df_formats.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level']).size().reset_index()
-    files = files.rename({0: 'File_Count'}, axis=1)
-    size = df_formats.groupby(['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level'])['FITS_Size_KB'].sum().reset_index()
-    size['Size_GB'] = size['FITS_Size_KB']/1000000
-    size = size.drop(columns='FITS_Size_KB')
-    df_formats = pd.merge(files, size, how='outer')
+    # Gets the number of files per format and GB per format, and combines into one dataframe.
+    df_files = files_per_format(df_formats)
+    df_size = size_per_format(df_formats)
+    df_format_list = pd.merge(df_files, df_size, how='outer')
 
     # Save the result to a CSV in directory.
-    df_formats.to_csv(os.path.join(directory, 'combined_format_data.csv'), index=False)
+    df_format_list.to_csv(os.path.join(directory, 'combined_format_data.csv'), index=False)
