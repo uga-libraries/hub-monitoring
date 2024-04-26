1834cf7cab2af4b71af6978f971ed0d432d7a308
Adriane Hanson <ahansonuga@gmail.com>
2024-04-26 14:39:36 -0400

    Sync existing tests for update_report to function change

--- tests/validate_fixity/test_update_report.py
+++ tests/validate_fixity/test_update_report.py
@@ -17,53 +17,53 @@ class MyTestCase(unittest.TestCase):
         if exists(report):
             remove(report)
 
-    def test_header(self):
-        """Test for when the report is made for the first time and the header is added"""
-        update_report(['Bag', 'Valid', 'Errors'], getcwd())
-
-        df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
-        report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Bag', 'Valid', 'Errors']]
-        self.assertEqual(report_rows, expected, 'Problem with test for header')
-
-    def test_not_valid(self):
+    def test_bag_not_valid(self):
         """Test for adding validation information for bags that are not valid to an existing report"""
-        update_report(['Bag', 'Valid', 'Errors'], getcwd())
+        update_report(['Accession', 'Valid', 'Errors'], getcwd())
         update_report(['2023_test003_001_er', False, 'Payload-Oxum validation failed.'], getcwd())
         update_report(['2023_test003_002_er', False, 'Bag validation failed.'], getcwd())
 
         df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
         df = df.fillna('nan')
         report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Bag', 'Valid', 'Errors'],
+        expected = [['Accession', 'Valid', 'Errors'],
                     ['2023_test003_001_er', False, 'Payload-Oxum validation failed.'],
                     ['2023_test003_002_er', False, 'Bag validation failed.']]
         self.assertEqual(report_rows, expected, 'Problem with test for validation')
 
-    def test_valid(self):
+    def test_bag_valid(self):
         """Test for adding validation information for bags that are valid to an existing report"""
-        update_report(['Bag', 'Valid', 'Errors'], getcwd())
+        update_report(['Accession', 'Valid', 'Errors'], getcwd())
         update_report(['2023_test003_001_er', True, None], getcwd())
         update_report(['2023_test003_002_er', True, None], getcwd())
 
         df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
         df = df.fillna('nan')
         report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Bag', 'Valid', 'Errors'],
+        expected = [['Accession', 'Valid', 'Errors'],
                     ['2023_test003_001_er', True, 'nan'],
                     ['2023_test003_002_er', True, 'nan']]
         self.assertEqual(report_rows, expected, 'Problem with test for validation')
 
+    def test_header(self):
+        """Test for when the report is made for the first time and the header is added"""
+        update_report(['Accession', 'Valid', 'Errors'], getcwd())
+
+        df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
+        report_rows = [df.columns.tolist()] + df.values.tolist()
+        expected = [['Accession', 'Valid', 'Errors']]
+        self.assertEqual(report_rows, expected, 'Problem with test for header')
+
     def test_valid_mix(self):
         """Test for adding validation information for bags that are and are not valid to an existing report"""
-        update_report(['Bag', 'Valid', 'Errors'], getcwd())
+        update_report(['Accession', 'Valid', 'Errors'], getcwd())
         update_report(['2023_test003_001_er', False, 'Payload-Oxum validation failed.'], getcwd())
         update_report(['2023_test003_002_er', True, None], getcwd())
 
         df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
         df = df.fillna('nan')
         report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Bag', 'Valid', 'Errors'],
+        expected = [['Accession', 'Valid', 'Errors'],
                     ['2023_test003_001_er', False, 'Payload-Oxum validation failed.'],
                     ['2023_test003_002_er', True, 'nan']]
         self.assertEqual(report_rows, expected, 'Problem with test for validation')
