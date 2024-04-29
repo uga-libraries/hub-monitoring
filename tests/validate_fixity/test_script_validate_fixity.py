ca942d3ce5c0b485a041940a86a387c792ed03db
Adriane Hanson <ahansonuga@gmail.com>
2024-04-29 10:03:15 -0400

    Sync script test with reorganized test data

--- tests/validate_fixity/test_script_validate_fixity.py
+++ tests/validate_fixity/test_script_validate_fixity.py
@@ -25,7 +25,7 @@ class MyTestCase(unittest.TestCase):
         # Reports
         directory = join(getcwd(), '..', 'test_data', 'Validate_Fixity', 'test_003_log_update')
         reports = [f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv",
-                   '2023_test003_004_er_manifest_validation_errors.csv']
+                   '2023_test003_003_er_manifest_validation_errors.csv']
         for report in reports:
             if exists(join(directory, report)):
                 remove(join(directory, report))
@@ -45,8 +45,8 @@ class MyTestCase(unittest.TestCase):
                     ['2023_test003_001_er_bag', False,
                      'Payload-Oxum validation failed. Expected 1 files and 4 bytes but found 1 files and 26 bytes'],
                     ['2023_test003_002_er_bag', True, 'nan'],
-                    ['2023_test003_003_er', True, '0 errors'],
-                    ['2023_test003_004_er', False, '6 errors']]
+                    ['2023_test003_003_er', False, '6 errors'],
+                    ['2023_test003_004_er', True, '0 errors']]
         self.assertEqual(report_rows, expected, 'Problem with test for correct, validation report')
 
         # Verifies the contents of the log for 2023_test003_001_er have been updated.
@@ -81,20 +81,34 @@ class MyTestCase(unittest.TestCase):
                      'Validated bag for accession 2023.3.2.ER. The bag is valid.', 'validate_fixity.py']]
         self.assertEqual(log_rows, expected, 'Problem with test for correct, 2023_test003_002_er')
 
-        # # Verifies the contents of the log for 2023_test003_003_er have been updated.
-        # # TODO: incorrectly putting Not valid in the log. Double checked the fixity in the manifest is correct.
-        # df = read_csv(join(directory, '2023_test003_003_er', 'preservation_log.txt'), delimiter='\t')
-        # df = df.fillna('nan')
-        # log_rows = [df.columns.tolist()] + df.values.tolist()
-        # expected = [['Collection', 'Accession', 'Date', 'Media Identifier', 'Action', 'Staff'],
-        #             ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD1', 'Virus scanned. No threats.', 'Jane Doe'],
-        #             ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD1', 'Copied. No errors.', 'Jane Doe'],
-        #             ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD2', 'Virus scanned. No threats.', 'Jane Doe'],
-        #             ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD2', 'Copied. No errors.', 'Jane Doe'],
-        #             ['TEST.3', '2023.3.3.ER', '2023-03-28', 'nan', 'Cannot bag. Made manifest. Valid.', 'Jane Doe'],
-        #             ['TEST.3', '2023.3.3.ER', date.today().strftime('%Y-%m-%d'), 'nan',
-        #              'Validated manifest for accession 2023.3.3.ER. The manifest is valid.', 'validate_fixity.py']]
-        # self.assertEqual(log_rows, expected, 'Problem with test for correct, 2023_test003_003_er')
+        # Verifies the contents of the log for 2023_test003_003_er have been updated.
+        df = read_csv(join(directory, '2023_test003_003_er', 'preservation_log.txt'), delimiter='\t')
+        df = df.fillna('nan')
+        log_rows = [df.columns.tolist()] + df.values.tolist()
+        expected = [['Collection', 'Accession', 'Date', 'Media Identifier', 'Action', 'Staff'],
+                    ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD1', 'Virus scanned. No threats.', 'Jane Doe'],
+                    ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD1', 'Copied. No errors.', 'Jane Doe'],
+                    ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD2', 'Virus scanned. No threats.', 'Jane Doe'],
+                    ['TEST.3', '2023.3.3.ER', '2023-03-28', 'CD2', 'Copied. No errors.', 'Jane Doe'],
+                    ['TEST.3', '2023.3.3.ER', '2023-03-28', 'nan', 'Cannot bag. Made manifest. Valid.', 'Jane Doe'],
+                    ['TEST.3', '2023.3.3.ER', date.today().strftime('%Y-%m-%d'), 'nan',
+                     'Validated manifest for accession 2023.3.3.ER. The manifest is not valid.', 'validate_fixity.py']]
+        self.assertEqual(log_rows, expected, 'Problem with test for correct, 2023_test003_003_er')
+
+        # Verifies the contents of 2023_test0003_004_er manifest validation errors log.
+        df = read_csv(join(directory, '2023_test003_003_er_manifest_validation_errors.csv'))
+        rows = [df.columns.tolist()] + df.values.tolist()
+        expected = [['File', 'MD5', 'MD5_Source'],
+                    ['Z:\\CD_2\\File02.txt', '0CBC6611F5540BD0809A388DC95A615B', 'Manifest'],
+                    ['Z:\\CD_2\\File03.txt', '3D77C578A138BA560F31DD22B83A53D3', 'Manifest'],
+                    ['Z:\\CD_1\\File1.txt', '4324B4C675E56A5E04BD9A8C74796EE5', 'Manifest'],
+                    [join(directory, '2023_test003_003_er', '2023_test003_003_er', 'CD_1', 'File1.txt'),
+                     '717216B472AA04EB2E615809C7F30C4E', 'Current'],
+                    [join(directory, '2023_test003_003_er', '2023_test003_003_er', 'CD_2', 'File02.txt'),
+                     '8078CD550FCF6755750A59378AFC7D30', 'Current'],
+                    [join(directory, '2023_test003_003_er', '2023_test003_003_er','CD_1', 'New Text Document.txt'),
+                     '9669CD9006F03AD6F1F8831601640482', 'Current']]
+        self.assertEqual(rows, expected, 'Problem with test for correct, manifest validation errors')
 
         # Verifies the contents of the log for 2023_test003_004_er have been updated.
         df = read_csv(join(directory, '2023_test003_004_er', 'preservation_log.txt'), delimiter='\t')
@@ -107,23 +121,8 @@ class MyTestCase(unittest.TestCase):
                     ['TEST.3', '2023.3.4.ER', '2023-04-28', 'CD2', 'Copied. No errors.', 'Jane Doe'],
                     ['TEST.3', '2023.3.4.ER', '2023-04-28', 'nan', 'Cannot bag. Made manifest. Valid.', 'Jane Doe'],
                     ['TEST.3', '2023.3.4.ER', date.today().strftime('%Y-%m-%d'), 'nan',
-                     'Validated manifest for accession 2023.3.4.ER. The manifest is not valid.', 'validate_fixity.py']]
+                     'Validated manifest for accession 2023.3.4.ER. The manifest is valid.', 'validate_fixity.py']]
         self.assertEqual(log_rows, expected, 'Problem with test for correct, 2023_test003_004_er')
-
-        # Verifies the contents of 2023_test0003_004_er manifest validation errors log.
-        df = read_csv(join(directory, '2023_test003_004_er_manifest_validation_errors.csv'))
-        rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['File', 'MD5', 'MD5_Source'],
-                    ['Z:\\CD_2\\File02.txt', '0CBC6611F5540BD0809A388DC95A615B', 'Manifest'],
-                    ['Z:\\CD_2\\File03.txt', '3D77C578A138BA560F31DD22B83A53D3', 'Manifest'],
-                    ['Z:\\CD_1\\File1.txt', '4324B4C675E56A5E04BD9A8C74796EE5', 'Manifest'],
-                    [join(directory, '2023_test003_004_er', '2023_test003_004_er', 'CD_1', 'File1.txt'),
-                     '717216B472AA04EB2E615809C7F30C4E', 'Current'],
-                    [join(directory, '2023_test003_004_er', '2023_test003_004_er', 'CD_2', 'File02.txt'),
-                     '8078CD550FCF6755750A59378AFC7D30', 'Current'],
-                    [join(directory, '2023_test003_004_er', '2023_test003_004_er','CD_1', 'New Text Document.txt'),
-                     '9669CD9006F03AD6F1F8831601640482', 'Current']]
-        self.assertEqual(rows, expected, 'Problem with test for correct, manifest validation errors')
 
     def test_error(self):
         """Test for when the script argument is not correct and the script exits"""
