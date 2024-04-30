196d24a252e83def2e7f054c7448fa81f44dabb2
Adriane Hanson <ahansonuga@gmail.com>
2024-04-30 09:01:17 -0400

    Removed Valid column from report in script test

--- tests/validate_fixity/test_update_report.py
+++ tests/validate_fixity/test_update_report.py
@@ -30,28 +30,15 @@ class MyTestCase(unittest.TestCase):
                     ['2023_test003_002_er', False, 'Bag validation failed.']]
         self.assertEqual(report_rows, expected, 'Problem with test for bag not valid')
 
-    def test_bag_valid(self):
-        """Test for adding validation information for bags that are valid to an existing report"""
-        update_report(['2023_test003_001_er', True, None], getcwd())
-        update_report(['2023_test003_002_er', True, None], getcwd())
-
-        df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
-        df = df.fillna('nan')
-        report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Accession', 'Valid', 'Errors'],
-                    ['2023_test003_001_er', True, 'nan'],
-                    ['2023_test003_002_er', True, 'nan']]
-        self.assertEqual(report_rows, expected, 'Problem with test for bag valid')
-
     def test_one_row(self):
         """Test for when the report is made for the first time"""
-        update_report(['2023_test003_001_er', True, None], getcwd())
+        update_report(['2023_test003_001_er', False, 'Bag validation failed.'], getcwd())
 
         df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
         df = df.fillna('nan')
         report_rows = [df.columns.tolist()] + df.values.tolist()
         expected = [['Accession', 'Valid', 'Errors'],
-                    ['2023_test003_001_er', True, 'nan']]
+                    ['2023_test003_001_er', False, 'Bag validation failed.']]
         self.assertEqual(report_rows, expected, 'Problem with test for one row')
 
     def test_manifest_not_valid(self):
@@ -68,20 +55,6 @@ class MyTestCase(unittest.TestCase):
                     ['2023_test003_003_er', False, '1 errors'],
                     ['2023_test003_004_er', False, '2 errors']]
         self.assertEqual(report_rows, expected, 'Problem with test for manifest not valid')
-
-    def test_manifest_valid(self):
-        """Test for adding validation information for manifests that are valid to an existing report"""
-        error = []
-        update_report(['2023_test003_003_er', True, f'{len(error)} errors'], getcwd())
-        update_report(['2023_test003_004_er', True, f'{len(error)} errors'], getcwd())
-
-        df = read_csv(join(getcwd(), f"fixity_validation_{date.today().strftime('%Y-%m-%d')}.csv"))
-        df = df.fillna('nan')
-        report_rows = [df.columns.tolist()] + df.values.tolist()
-        expected = [['Accession', 'Valid', 'Errors'],
-                    ['2023_test003_003_er', True, '0 errors'],
-                    ['2023_test003_004_er', True, '0 errors']]
-        self.assertEqual(report_rows, expected, 'Problem with test for manifest valid')
 
     def test_mix(self):
         """Test for adding validation information for accessions that are and are not valid to an existing report"""
