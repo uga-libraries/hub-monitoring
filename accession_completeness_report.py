22a2dc7a222b84b6fef5fade823784799f45ad33
Adriane Hanson <ahansonuga@gmail.com>
2024-04-30 10:47:25 -0400

    Make accession_paths()
    Finds all Hargrett accessions. Some edge cases are temporarily hard-coded.

--- accession_completeness_report.py
+++ accession_completeness_report.py
@@ -6,33 +6,68 @@ import os
 import sys
 
 
+def accession_paths(coll_dir, coll):
+    """Find the accession folder(s), which can be in a few places in the collection folder
+
+    coll_dir (string): path to the folder with collections (script argument)
+    coll (string): folder name of the collection
+
+    returns a list of paths to the folders with the accession content,
+    usually named with the accession number but may be the collection or Preservation Copies
+    """
+
+    acc_paths = []
+
+    # First level within the collection is either the content, accession folders
+    # or a folder "Preservation Copies" with the content or accession folders.
+    for acc in os.listdir(os.path.join(coll_dir, coll)):
+
+        # Skip files.
+        if os.path.isfile(os.path.join(coll_dir, coll, acc)):
+            continue
+
+        # Skip non-accession folders that may be sibling folders of accessions.
+        if acc == 'Risk_remediation' or acc == 'Access Copies' or acc.startswith('Apprais') or acc.endswith('FITS'):
+            continue
+
+        # Accession folders that are inside Preservation Copies folders.
+        if os.path.exists(os.path.join(coll_dir, coll, 'Preservation Copies')):
+            # Content is in Preservation Copies.
+            # TODO: do not depend on knowing the coll.
+            if coll in ['ua12-022 GLOBES records', 'ua16-010 Athens Music Project collection']:
+                acc_paths.append(os.path.join(coll_dir, coll, 'Preservation Copies'))
+            # Accession folders are in Preservation Copies.
+            else:
+                for accession_folder in os.listdir(os.path.join(coll_dir, coll, 'Preservation Copies')):
+                    acc_paths.append(os.path.join(coll_dir, coll, 'Preservation Copies', accession_folder))
+        # Accessions content inside the collection folder.
+        # TODO: do not depend on knowing the coll.
+        elif coll == 'ms4466 Athens Metal Arts Guild':
+            acc_paths.append(os.path.join(coll_dir, coll))
+        # Accession folders that are inside the collection folder.
+        else:
+            acc_paths.append(os.path.join(coll_dir, coll, acc))
+
+    return acc_paths
+
+
 if __name__ == '__main__':
-    directory = sys.argv[1]
+    collection_directory = sys.argv[1]
 
     # First level within the folder is the collection.
-    for collection in os.listdir(directory):
+    for collection in os.listdir(collection_directory):
 
-        # Skip files and non-collection folder
-        if os.path.isfile(os.path.join(directory, collection)) or collection == 'scripts':
+        # Skip files and the non-collection folder
+        if os.path.isfile(os.path.join(collection_directory, collection)) or collection == 'scripts':
+            continue
+
+        # Skip Linguistic Atlas Project, which is handled differently.
+        # TODO: confirm this
+        if collection == 'ua22-008 Linguistic Atlas Project':
             continue
 
         print()
         print(collection)
 
-        # First level within the collection is either the accessions or a folder "Preservation Copies" with accessions.
-        for accession in os.listdir(os.path.join(directory, collection)):
-
-            # Skip files.
-            if os.path.isfile(os.path.join(directory, collection, accession)):
-                continue
-
-            # Skip non-accession folders that are siblings of accessions.
-            if accession == 'Risk_remediation' or accession.startswith('Apprais') or accession.endswith('FITS'):
-                continue
-
-            # Gets accessions that are inside Preservation copies.
-            if os.path.exists(os.path.join(directory, collection, 'Preservation Copies')):
-                for accession_folder in os.listdir(os.path.join(directory, collection, 'Preservation Copies')):
-                    print(accession_folder)
-            else:
-                print("access in collection folder")
+        accession_list = accession_paths(collection_directory, collection)
+        print(accession_list)
