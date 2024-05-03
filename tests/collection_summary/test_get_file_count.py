b7a31abb055b7b82029b3e2dbb0a91eb3a1ec87a
Adriane Hanson <ahansonuga@gmail.com>
2024-05-03 08:54:52 -0400

    Move test data into tests/collection_summary
    Simplifies the paths to the test data within the unit tests.

--- tests/collection_summary/test_get_file_count.py
+++ tests/collection_summary/test_get_file_count.py
@@ -4,43 +4,38 @@ The directory may contain folders.
 """
 import unittest
 from collection_summary import get_file_count
-from os import getcwd
 from os.path import join
 
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self):
-        """Path to the department hub, used in all tests."""
-        self.dept_hub = join(getcwd(), '..', 'test_data', 'Collection_Summary', 'Russell_Hub')
-
     def test_file(self):
         """Test for a directory with one file and no folders"""
-        acc_path = join(self.dept_hub, 'backlog', 'rbrl001', '2015-01-er')
+        acc_path = join('test_data', 'Russell_Hub', 'backlog', 'rbrl001', '2015-01-er')
         files = get_file_count(acc_path)
         self.assertEqual(files, 1, "Problem with test for one file")
 
     def test_file_folder(self):
         """Test for a directory with one file, which is in a folder"""
-        acc_path = join(self.dept_hub, 'backlog', 'rbrl001', '2015-12-er')
+        acc_path = join('test_data', 'Russell_Hub', 'backlog', 'rbrl001', '2015-12-er')
         files = get_file_count(acc_path)
         self.assertEqual(files, 1, "Problem with test for one file in a folder")
 
     def test_files(self):
         """Test for a directory with two files and no folders"""
-        acc_path = join(self.dept_hub, 'backlog', 'rbrl001', '2016-03-er')
+        acc_path = join('test_data', 'Russell_Hub', 'backlog', 'rbrl001', '2016-03-er')
         files = get_file_count(acc_path)
         self.assertEqual(files, 2, "Problem with test for two files")
 
     def test_files_folders(self):
         """Test for a directory with multiple files in multiple folders"""
-        acc_path = join(self.dept_hub, 'backlog', 'rbrl001', '2018-04-er')
+        acc_path = join('test_data', 'Russell_Hub', 'backlog', 'rbrl001', '2018-04-er')
         files = get_file_count(acc_path)
         self.assertEqual(files, 5, "Problem with test for multiple files in multiple folders")
 
     def test_unbagged(self):
         """Test for an accession that is not in a bag"""
-        acc_path = join(self.dept_hub, 'backlog', 'rbrl001', '2019-12-er')
+        acc_path = join('test_data', 'Russell_Hub', 'backlog', 'rbrl001', '2019-12-er')
         files = get_file_count(acc_path)
         self.assertEqual(files, 2, "Problem with test for unbagged")
 
--- tests/collection_summary/test_get_size.py
+++ tests/collection_summary/test_get_size.py
@@ -4,43 +4,38 @@ The directory may contain folders
 """
 import unittest
 from collection_summary import get_size
-from os import getcwd
 from os.path import join
 
 
 class MyTestCase(unittest.TestCase):
-
-    def setUp(self):
-        """Path to the department hub, used in all tests."""
-        self.dept_hub = join(getcwd(), '..', 'test_data', 'Collection_Summary', 'Russell_Hub')
         
     def test_file(self):
         """Test for a directory with one file and no folders"""
-        acc_path = join(self.dept_hub, 'closed', 'rbrl003', '2022-27-er')
+        acc_path = join('test_data', 'Russell_Hub', 'closed', 'rbrl003', '2022-27-er')
         size = get_size(acc_path)
         self.assertEqual(size, 0.00001, "Problem with test for one file")
 
     def test_file_folder(self):
         """Test for a directory with three files, some of which are in a folder"""
-        acc_path = join(self.dept_hub, 'closed', 'rbrl003', '2023-01-er')
+        acc_path = join('test_data', 'Russell_Hub', 'closed', 'rbrl003', '2023-01-er')
         size = get_size(acc_path)
         self.assertEqual(size, 0.00001, "Problem with test one file in a folder")
 
     def test_files(self):
         """Test for a directory with two files and no folders"""
-        acc_path = join(self.dept_hub, 'closed', 'rbrl003', '2023-12-er')
+        acc_path = join('test_data', 'Russell_Hub', 'closed', 'rbrl003', '2023-12-er')
         size = get_size(acc_path)
         self.assertEqual(size, 0.0001, "Problem with test for two files")
 
     def test_files_folders(self):
         """Test for a directory with multiple files and multiple folders"""
-        acc_path = join(self.dept_hub, 'closed', 'rbrl003', '2023-23-er')
+        acc_path = join('test_data', 'Russell_Hub', 'closed', 'rbrl003', '2023-23-er')
         size = get_size(acc_path)
         self.assertEqual(size, 0.0005, "Problem with test for folder")
 
     def test_unbagged(self):
         """Test for an accession that is not in a bag"""
-        acc_path = join(self.dept_hub, 'closed', 'rbrl003', '2019-13-er')
+        acc_path = join('test_data', 'Russell_Hub', 'closed', 'rbrl003', '2019-13-er')
         size = get_size(acc_path)
         self.assertEqual(size, 0.0002, "Problem with test for unbagged")
 
