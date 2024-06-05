605a22624ca05b3e4ab399f572947451ed8f8a84
Adriane Hanson <ahansonuga@gmail.com>
2024-06-05 12:17:13 -0400

    Add test for preservation log

--- tests/validate_fixity/test_validate_bag_manifest.py
+++ tests/validate_fixity/test_validate_bag_manifest.py
@@ -2,13 +2,13 @@
 Start for tests for the function validate_bag_manifest(), which validates an accession using the bag manifest
 if bagit cannot do the validation.
 
-At this point, it is just running a valid and not valid bag through the function for initial development,
-with print statements in the function showing the value.
-Once we confirm this is working on real data, these will be made into real tests.
+These tests use bags that would not cause a bagit error, since that is not reliably replicable.
+This function does not depend on the error, so it can use a normal bag.
 """
 import unittest
 from validate_fixity import validate_bag_manifest
-from os import getcwd
+from test_script_validate_fixity import csv_to_list
+from datetime import date
 from os.path import join
 from shutil import copyfile
 
@@ -19,8 +19,8 @@ class MyTestCase(unittest.TestCase):
         """Return the preservation logs to the original contents after testing,
         using a copy of the original log that is also in the accession folder"""
         # List of paths for accessions with logs to replace.
-        accessions = [join(getcwd(), 'test_data', 'test_001_bags_valid', '2023_test001_002_er'),
-                      join(getcwd(), 'test_data', 'test_002_bags_invalid', '2023_test002_001_er')]
+        accessions = [join('test_data', 'test_001_bags_valid', '2023_test001_002_er'),
+                      join('test_data', 'test_002_bags_invalid', '2023_test002_001_er')]
 
         # For each accession, replaces the updated log with a copy of the original log from the accession folder.
         for accession in accessions:
@@ -28,15 +28,57 @@ class MyTestCase(unittest.TestCase):
 
     def test_not_valid(self):
         """Test for when the bag is not valid"""
-        bag_dir = join(getcwd(), 'test_data', 'test_002_bags_invalid', '2023_test002_001_er', '2023_test002_001_er_bag')
+        # Makes the variable needed for function input and runs the function.
+        bag_dir = join('test_data', 'test_002_bags_invalid', '2023_test002_001_er', '2023_test002_001_er_bag')
         validate_bag_manifest(bag_dir)
-        self.assertEqual(False, False)  # add assertion here
 
-    # def test_valid(self):
-    #     """Test for when the bag is valid"""
-    #     bag_dir = join(getcwd(), 'test_data', 'test_001_bags_valid', '2023_test001_002_er', '2023_test001_002_er_bag')
-    #     validate_bag_manifest(bag_dir)
-    #     self.assertEqual(True, True)  # add assertion here
+        # Verifies the contents of the preservation_log.txt have been updated.
+        result = csv_to_list(join('test_data', 'test_002_bags_invalid', '2023_test002_001_er', 'preservation_log.txt'), delimiter='\t')
+        expected = [['Collection', 'Accession', 'Date', 'Media Identifier', 'Action', 'Staff'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.001',
+                     'Virus scanned using Microsoft Defender. No security threats were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.001',
+                     'Copied to external storage device using TeraCopy. No errors were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.001',
+                     'Bagged with accession 2023.test002.001.ER. No errors were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.002',
+                     'Virus scanned using Microsoft Defender. No security threats were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.002',
+                     'Copied to external storage device using TeraCopy. No errors were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'CD.002',
+                     'Bagged with accession 2023.test002.001.ER. No errors were detected.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', '2023-01-31', 'nan',
+                     'Validated bag for accession 2023.test002.001.ER. The bag was valid.', 'Jane Doe'],
+                    ['TEST.002', '2023.test002.001.ER', date.today().strftime('%Y-%m-%d'), 'nan',
+                     'Validated bag for accession 2023.test002.001.ER. The bag is not valid.', 'validate_fixity.py']]
+        self.assertEqual(result, expected, 'Problem with test for not valid, preservation_log.txt')
+
+    def test_valid(self):
+        """Test for when the bag is valid"""
+        # Makes the variable needed for function input and runs the function.
+        bag_dir = join('test_data', 'test_001_bags_valid', '2023_test001_002_er', '2023_test001_002_er_bag')
+        validate_bag_manifest(bag_dir)
+
+        # Verifies the contents of the preservation_log.txt have been updated.
+        result = csv_to_list(join('test_data', 'test_001_bags_valid', '2023_test001_002_er', 'preservation_log.txt'), delimiter='\t')
+        expected = [['Collection', 'Accession', 'Date', 'Media Identifier', 'Action', 'Staff'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-14', 'CD.001',
+                     'Virus scanned using Microsoft Defender. No security threats were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-14', 'CD.001',
+                     'Copied to external storage device using TeraCopy. No errors were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-14', 'CD.001',
+                     'Bagged with accession 2023.test001.002.ER. No errors were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-15', 'CD.002',
+                     'Virus scanned using Microsoft Defender. No security threats were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-15', 'CD.002',
+                     'Copied to external storage device using TeraCopy. No errors were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-15', 'CD.002',
+                     'Bagged with accession 2023.test001.002.ER. No errors were detected.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', '2023-11-15', 'nan',
+                     'Validated bag for accession 2023.test001.002.ER. The bag was valid.', 'Jane Doe'],
+                    ['TEST.001', '2023.test001.002.ER', date.today().strftime('%Y-%m-%d'), 'nan',
+                     'Validated bag for accession 2023.test001.002.ER. The bag is valid.', 'validate_fixity.py']]
+        self.assertEqual(result, expected, 'Problem with test for valid, preservation_log.txt')
 
 
 if __name__ == '__main__':
