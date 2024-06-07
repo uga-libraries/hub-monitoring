066c92136315f758e833b6810b394c9e170f4b60
Adriane Hanson <ahansonuga@gmail.com>
2024-06-07 10:56:40 -0400

    Update test_005 folder name

--- tests/validate_fixity/test_manifest_validation_log.py
+++ tests/validate_fixity/test_manifest_validation_log.py
@@ -14,13 +14,13 @@ class MyTestCase(unittest.TestCase):
 
     def tearDown(self):
         """Delete the test output if it was created"""
-        if exists(join('test_data', 'test_005_manifest_invalid', '2023_test005_001_er_manifest_validation_errors.csv')):
-            remove(join('test_data', 'test_005_manifest_invalid', '2023_test005_001_er_manifest_validation_errors.csv'))
+        if exists(join('test_data', 'test_005_manifest_not_valid', '2023_test005_001_er_manifest_validation_errors.csv')):
+            remove(join('test_data', 'test_005_manifest_not_valid', '2023_test005_001_er_manifest_validation_errors.csv'))
 
     def test_both(self):
         """Test for when the errors are from file count and fixity differences"""
         # Makes variables for function input and runs the function.
-        directory = join('test_data', 'test_005_manifest_invalid')
+        directory = join('test_data', 'test_005_manifest_not_valid')
         root = join(directory, '2023_test005_001_er')
         errors_list = [['Z:\\2023_test005_002_er\\CD_2\\File02.txt', '0CBC6611F5540BD0809A388DC95A615B', 'Manifest'],
                        [join(root, 'CD_2', 'File02.txt'), '8078CD550FCF6755750A59378AFC7D30', 'Current'],
@@ -38,7 +38,7 @@ class MyTestCase(unittest.TestCase):
     def test_files(self):
         """Test for when the errors are from file count differences"""
         # Makes variables for function input and runs the function.
-        directory = join('test_data', 'test_005_manifest_invalid')
+        directory = join('test_data', 'test_005_manifest_not_valid')
         root = join(directory, '2023_test005_001_er')
         errors_list = [['Number of files does not match. 3 files in the accession folder and 4 in the manifest.']]
         manifest_validation_log(directory, basename(root), errors_list)
@@ -52,7 +52,7 @@ class MyTestCase(unittest.TestCase):
     def test_fixity(self):
         """Test for when the errors are from fixity differences"""
         # Makes variables for function input and runs the function.
-        directory = join('test_data', 'test_005_manifest_invalid')
+        directory = join('test_data', 'test_005_manifest_not_valid')
         root = join(directory, '2023_test005_001_er')
         errors_list = [['Z:\\2023_test005_002_er\\CD_2\\File02.txt', '0CBC6611F5540BD0809A388DC95A615B', 'Manifest'],
                        ['Z:\\2023_test005_002_er\\CD_1\\File1.txt', '4324B4C675E56A5E04BD9A8C74796EE5', 'Manifest'],
