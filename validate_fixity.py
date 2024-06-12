bc516084d56b6811ea9766ee52cba7f210907eb6
Adriane Hanson <ahansonuga@gmail.com>
2024-06-12 08:54:47 -0400

    Temp error handling if can't get MD5

--- validate_fixity.py
+++ validate_fixity.py
@@ -291,10 +291,14 @@ def validate_manifest(acc_dir, manifest, report_dir):
     for root, dirs, files in os.walk(acc_files):
         for file in files:
             filepath = os.path.join(root, file)
-            with open(filepath, 'rb') as f:
-                data = f.read()
-                md5_generated = hashlib.md5(data).hexdigest()
-            files_list.append([filepath, md5_generated.upper()])
+            #TODO: temporary to get through testing
+            try:
+                with open(filepath, 'rb') as f:
+                    data = f.read()
+                    md5_generated = hashlib.md5(data).hexdigest()
+                files_list.append([filepath, md5_generated.upper()])
+            except FileNotFoundError:
+                print('Cannot get md5 for file in ', acc_dir)
     df_files = pd.DataFrame(files_list, columns=['Acc_Path', 'Acc_MD5'], dtype=object)
 
     # Reads the manifest into a dataframe.
