e2013bd74a533ddbee2d82eb5e2ca60118aa9320
Adriane Hanson <ahansonuga@gmail.com>
2024-09-05 16:39:01 -0400

    Test for script, all complete

--- tests/accession_completeness_report/test_script_accession_completeness_report.py
+++ tests/accession_completeness_report/test_script_accession_completeness_report.py
@@ -28,6 +28,26 @@ class MyTestCase(unittest.TestCase):
         if exists(join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv')):
             remove(join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv'))
 
+    def test_complete(self):
+        """Test for an input directory that contains only complete accessions, so no report is generated"""
+        # Makes the variable used for script input and runs the script.
+        script = join(getcwd(), '..', '..', 'accession_completeness_report.py')
+        input_directory = join('script_test_data', 'complete')
+        script_message = subprocess.run(f'python {script} {input_directory}', shell=True, stdout=subprocess.PIPE)
+
+        # Tests the correct message is printed.
+        today = date.today().strftime('%Y-%m-%d')
+        result = script_message.stdout.decode('utf-8')
+        expected = (f"Starting on accession {join(input_directory, 'backlogged', 'coll_1', 'acc_1_1')}\r\n"
+                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_1')}\r\n"
+                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_2')}\r\n"
+                    f"\r\nAll accessions are complete.\r\n")
+        self.assertEqual(result, expected, "Problem with test for complete, printed message")
+
+        # Tests the csv was not made.
+        result = exists(join(input_directory, f'accession_completeness_report_{today}.csv'))
+        self.assertEqual(result, False, "Problem with test for complete, report")
+
     def test_incomplete(self):
         """Test for an input directory that contains some incomplete accessions, so the report is generated
         but does not include the complete accessions or an accession from the unconventional collection list"""
