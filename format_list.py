f9cc7c721abae51b64bb636a94d86add689ef84c
Adriane Hanson <ahansonuga@gmail.com>
2024-09-06 11:27:15 -0400

    Rename NARA risk column first
    Prevents error from selecting a column that might not exist, since some risk CSVs have Risk_Level and others Risk Level

--- format_list.py
+++ format_list.py
@@ -60,12 +60,13 @@ def df_cleanup(df):
     df (pandas DataFrame): dataframe with a subset of cleaned data from the most recent risk csv for every accession
     """
 
+    # Renames the only column with spaces to use underscores instead.
+    # In more recent risk csvs, it is already NARA_Risk_Level, so this has no impact.
+    df = df.rename({'NARA_Risk Level': 'NARA_Risk_Level'}, axis=1)
+
     # Keeps only the needed columns.
     # The other FITs data, NARA data, technical appraisal, and other risk columns are not used.
-    df = df[['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB', 'NARA_Risk Level']]
-
-    # Renames the only column with spaces to use underscores instead.
-    df = df.rename({'NARA_Risk Level': 'NARA_Risk_Level'}, axis=1)
+    df = df[['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB', 'NARA_Risk_Level']]
 
     # Removes duplicates from multiple NARA matches with the same NARA risk level to the same file.
     # The file will still be repeated once per NARA risk level.
