0f88b003b6ec3ec3aa1c00846cfee68d4d1df1b4
Adriane Hanson <ahansonuga@gmail.com>
2024-09-06 12:01:12 -0400

    Test for repeats

--- tests/format_list/test_files_per_format.py
+++ tests/format_list/test_files_per_format.py
@@ -1,31 +1,33 @@
 """
-Test for the function files_per_format(), which gets the number of files per format name/version/risk combination.
+Tests for the function files_per_format(), which gets the number of files per format name/version/risk combination.
+To simplify the test input, tests have the columns needed for that test.
 """
 import unittest
-from format_list import combine_risk_csvs, df_cleanup, files_per_format
+from format_list import files_per_format
 from test_df_cleanup import df_to_list
-from os import getcwd
-from os.path import join
+from pandas import DataFrame
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_function(self):
-        input_directory = join(getcwd(), 'test_data')
-        df_all = combine_risk_csvs(input_directory)
-        df_formats = df_cleanup(df_all)
+    def test_repeats(self):
+        """Test for combining repeating name, version, risk combinations"""
+        df_formats = DataFrame([['path1', 'format1', 'v1', 111, 'Moderate Risk'],
+                                ['path2', 'format1', 'v1', 112, 'Moderate Risk'],
+                                ['path3', 'format1', 'v2', 221, 'Low Risk'],
+                                ['path4', 'format1', 'v2', 222, 'Low Risk'],
+                                ['path5', 'format1', 'v2', 331, 'No Match'],
+                                ['path6', 'format1', 'v2', 332, 'No Match'],
+                                ['path7', 'format1', 'v2', 333, 'No Match']],
+                               columns=['FITS_File_Path', 'FITS_Format_Name', 'FITS_Format_Version', 'FITS_Size_KB',
+                                        'NARA_Risk_Level'])
         df_files = files_per_format(df_formats)
 
         result = df_to_list(df_files)
         expected = [['FITS_Format_Name', 'FITS_Format_Version', 'NARA_Risk_Level', 'File_Count'],
-                    ['JPEG File Interchange Format', '1.01', 'Low Risk', 2],
-                    ['JPEG File Interchange Format', '1.02', 'Low Risk', 3],
-                    ['PDF/A', '1b', 'Low Risk', 1],
-                    ['Plain text', 'no-version', 'Moderate Risk', 1],
-                    ['Portable Document Format', '1.4', 'Moderate Risk', 2],
-                    ['Portable Network Graphics', '1', 'High Risk', 1],
-                    ['Portable Network Graphics', '1', 'Moderate Risk', 1],
-                    ['Unknown Binary', 'no-version', 'No Match', 1]]
+                    ['format1', 'v1', 'Moderate Risk', 2],
+                    ['format1', 'v2', 'Low Risk', 2],
+                    ['format1', 'v2', 'No Match', 3]]
         self.assertEqual(result, expected)
 
 
