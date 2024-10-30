3cad2ee32edaadbd664223e7e8c1b4c41cb168a5
Adriane Hanson <ahansonuga@gmail.com>
2024-10-30 15:47:22 -0400

    Create test_check_restart.py

---
+++ tests/validate_fixity/test_check_restart.py
@@ -0,0 +1,32 @@
+"""
+Tests for the function check_restart(), which determines if the script is being restarted.
+"""
+import os
+import unittest
+from validate_fixity import check_restart
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_not_restart(self):
+        """Test for when the script is not being restarted (log is not present)"""
+        # Makes the variable for function input and runs the function.
+        input_directory = os.path.join('test_data', 'restart_no')
+        fixity_validation_log_path = check_restart(input_directory)
+
+        # Verifies fixity_validation_log_path has the correct value.
+        self.assertEqual(fixity_validation_log_path, None, "Problem with test for not restart")
+
+    def test_restart(self):
+        """Test for when the script is being restarted (log is present)"""
+        # Makes the variable for function input and runs the function.
+        input_directory = os.path.join('test_data', 'restart_yes')
+        fixity_validation_log_path = check_restart(input_directory)
+
+        # Verifies fixity_validation_log_path has the correct value.
+        expected = os.path.join('test_data', 'restart_yes', 'fixity_validation_log_20241031.csv')
+        self.assertEqual(fixity_validation_log_path, expected, "Problem with test for restart")
+
+
+if __name__ == '__main__':
+    unittest.main()
