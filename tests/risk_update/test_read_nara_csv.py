d0049dc404d54b3a25bdc502ce33c9735f773093
Adriane Hanson <ahansonuga@gmail.com>
2024-11-11 16:26:56 -0500

    Use separate nara csv to test reading
    Has all columns, not just a sample, and follows the full naming convention. The main goal is to not have to update multiple unit tests when we change the nara data for a test.

--- tests/risk_update/test_read_nara_csv.py
+++ tests/risk_update/test_read_nara_csv.py
@@ -12,38 +12,35 @@ class MyTestCase(unittest.TestCase):
     def test_current_nara(self):
         """Test for when the column names in the NARA CSV for the 5 columns used are correct, as of June 2024"""
         # Creates input variable (in production, this is a script argument) and runs the function.
-        nara_csv = os.path.join('test_data', 'NARA_PreservationActionPlan.csv')
+        # This spreadsheet has all the NARA columns but only 8 rows of data.
+        nara_csv = os.path.join('test_data', 'NARA_PreservationActionPlan_FileFormats_20240325.csv')
         nara_risk_df = read_nara_csv(nara_csv)
 
         # Tests the contents of nara_risk_df is correct.
         result = df_to_list(nara_risk_df)
         expected = [['NARA_Format_Name', 'NARA_File_Extensions', 'NARA_PRONOM_URL', 'NARA_Risk_Level',
                      'NARA_Proposed_Preservation_Plan'],
-                    ['Hypertext Markup Language 5.2', 'htm|html', 'https://www.nationalarchives.gov.uk/pronom/fmt/96',
+                    ['Graphics Interchange Format 87a', 'gif', 'https://www.nationalarchives.gov.uk/pronom/fmt/3',
+                     'Moderate Risk', 'Retain'],
+                    ['Graphics Interchange Format 89a', 'gif', 'https://www.nationalarchives.gov.uk/pronom/fmt/4',
                      'Low Risk', 'Retain'],
-                    ['Hypertext Markup Language unspecified version', 'htm|html',
-                     'https://www.nationalarchives.gov.uk/pronom/fmt/96', 'Low Risk', 'Retain'],
-                    ['National Imagery Transmission Format 1.1', 'ntf|nitf', 'nan', 'Low Risk',
-                     'Transform to a TBD format'],
-                    ['National Imagery Transmission Format unspecified version', 'ntf|nitf', 'nan', 'Low Risk',
-                     'Transform to a TBD format'],
-                    ['Portable Document Format (PDF) Portfolio 2.0', 'pdf', 'nan', 'Moderate Risk',
-                     'Retain but possibly extract files from the container'],
-                    ['Portable Document Format (PDF) version 1.0', 'pdf',
-                     'https://www.nationalarchives.gov.uk/pronom/fmt/14', 'Moderate Risk', 'Retain'],
-                    ['Portable Document Format/Archiving (PDF/A-1a) accessible', 'pdf',
-                     'https://www.nationalarchives.gov.uk/pronom/fmt/95', 'Low Risk', 'Retain'],
-                    ['Rich Text Format 1.5', 'rtf', 'https://www.nationalarchives.gov.uk/pronom/fmt/50',
-                     'Moderate Risk', 'Transform to PDF'],
-                    ['Rich Text Format 1.6', 'rtf', 'https://www.nationalarchives.gov.uk/pronom/fmt/50',
-                     'Moderate Risk', 'Transform to PDF'],
-                    ['WordPerfect Template', 'wpt', 'nan', 'Moderate Risk', 'Transform to PDF if possible']]
+                    ['Graphics Interchange Format unspecified version', 'gif', 'nan', 'Moderate Risk', 'Retain'],
+                    ['GZIP', 'gz|tgz', 'https://www.nationalarchives.gov.uk/pronom/x-fmt/266', 'Moderate Risk',
+                     'Retain but extract files from the container'],
+                    ['JPEG 2000 File Format', 'jp2|jpx', 'https://www.nationalarchives.gov.uk/pronom/x-fmt/392',
+                     'Low Risk', 'Retain'],
+                    ['JPEG File Interchange Format 1.00', 'jpg|jpeg|jpe',
+                     'https://www.nationalarchives.gov.uk/pronom/fmt/42', 'Low Risk', 'Retain'],
+                    ['JPEG File Interchange Format 1.01', 'jpg|jpeg|jpe',
+                     'https://www.nationalarchives.gov.uk/pronom/fmt/43', 'Low Risk', 'Retain'],
+                    ['JPEG File Interchange Format 1.02', 'jpg|jpeg|jpe',
+                     'https://www.nationalarchives.gov.uk/pronom/fmt/44', 'Low Risk', 'Retain']]
         self.assertEqual(result, expected, "Problem with test for current NARA")
 
     def test_incorrect_nara(self):
         """Test for when the column names in the NARA CSV for the 5 columns used are not correct"""
         # Creates input variable (in production, this is a script argument).
-        nara_csv = os.path.join('test_data', 'NARA_PreservationActionPlan_Outdated.csv')
+        nara_csv = os.path.join('test_data', 'NARA_PreservationActionPlan_FileFormats_19990125.csv')
 
         # Runs the function and verifies it raises the expected error.
         with self.assertRaises(KeyError):
