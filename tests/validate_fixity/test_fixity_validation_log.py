82109819d523e8147aad4fac302c0c98485bef0d
Adriane M Hanson <ahansonuga@gmail.com>
2025-06-02 12:40:57 -0400

    Tests for logs - accessions without fixity

--- tests/validate_fixity/test_fixity_validation_log.py
+++ tests/validate_fixity/test_fixity_validation_log.py
@@ -93,8 +93,23 @@ class MyTestCase(unittest.TestCase):
                     ['backlogged', 'ua22-333 records', 'Appraisal copy',
                      os.path.join(acc_dir, 'backlogged', 'ua22-333 records', 'Appraisal copy'),
                      'BLANK', 'BLANK', 'Not an accession'],
-                    ['closed', 'rbrl333', 'access', os.path.join(acc_dir, 'closed', 'rbrl333', 'access'),
+                    ['closed', 'harg1234', 'access', os.path.join(acc_dir, 'closed', 'harg1234', 'access'),
                      'BLANK', 'BLANK', 'Not an accession']]
+        self.assertEqual(result, expected, "Problem with test for no_acc")
+
+    def test_no_fixity(self):
+        """Test for when there are folders that are accessions but don't have fixity information  (included in log)"""
+        # Makes the variable for function input and runs the function.
+        acc_dir = os.path.join('test_data', 'log', 'no_fixity', 'born-digital')
+        fixity_validation_log(acc_dir)
+
+        # Verifies the log has the correct values.
+        result = csv_to_list(os.path.join(acc_dir, f"fixity_validation_log_{date.today().strftime('%Y-%m-%d')}.csv"))
+        expected = [['Status', 'Collection', 'Accession', 'Accession_Path', 'Fixity_Type', 'Fixity', 'Result'],
+                    ['closed', 'rbrl333', '2002_02_er', os.path.join(acc_dir, 'closed', 'rbrl333', '2002_02_er'),
+                     'BLANK', 'BLANK', 'No fixity information'],
+                    ['closed', 'rbrl333', 'no-acc-num', os.path.join(acc_dir, 'closed', 'rbrl333', 'no-acc-num'),
+                     'BLANK', 'BLANK', 'No fixity information']]
         self.assertEqual(result, expected, "Problem with test for no_acc")
 
 
