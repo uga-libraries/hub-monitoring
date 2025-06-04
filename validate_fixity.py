47a7fa47a4c34c8b9999bf96350234b0c55af8ff
Adriane M Hanson <ahansonuga@gmail.com>
2025-06-04 16:09:29 -0400

    Add Valid_Time column to fixity validation log

--- validate_fixity.py
+++ validate_fixity.py
@@ -110,7 +110,8 @@ def fixity_validation_log(acc_dir):
     Accession_Path is the full path to the folder which contains the digital content.
     Fixity_Type is Bag or Zip and will be used to determine how to validate the accession, or None.
     Fixity is the name of the bag folder or zip md5 file with the fixity information, or None.
-    Valid is None and will be updated with True, False, or Skipped after validation.
+    Valid is None or has text if Result also has text.
+    Valid_Time is None for all.
     Result is None for accessions to validate, "Not an accession", or "No fixity".
 
     There are other folders frequently at the accession level, such as FITS files and copies for appraisal.
@@ -128,7 +129,8 @@ def fixity_validation_log(acc_dir):
     log_path = os.path.join(acc_dir, f"fixity_validation_log_{date.today().strftime('%Y-%m-%d')}.csv")
     with (open(log_path, 'w', newline='') as open_log):
         log_writer = csv.writer(open_log)
-        log_writer.writerow(['Status', 'Collection', 'Accession', 'Accession_Path', 'Fixity_Type', 'Fixity', 'Valid', 'Result'])
+        log_writer.writerow(['Status', 'Collection', 'Accession', 'Accession_Path', 'Fixity_Type', 'Fixity',
+                             'Valid', 'Valid_Time', 'Result'])
 
         # Navigates the input_directory to get information about each folder at the accession level and adds to the log.
         for status in os.listdir(acc_dir):
@@ -165,7 +167,8 @@ def fixity_validation_log(acc_dir):
                                     is_valid = 'Skipped'
                                     result = 'Not an accession'
                                 # Adds information for folder to the log.
-                                row = [status, collection, folder, accession_path, fixity_type, fixity, is_valid, result]
+                                row = [status, collection, folder, accession_path, fixity_type, fixity,
+                                       is_valid, None, result]
                                 log_writer.writerow(row)
 
 
