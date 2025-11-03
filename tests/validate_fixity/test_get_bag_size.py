721d1bdc049623b19d67b47fcea063500174679b
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-03 09:29:03 -0500

    Test sizes more than 0 by updating bag-info.txt

--- tests/validate_fixity/test_get_bag_size.py
+++ tests/validate_fixity/test_get_bag_size.py
@@ -1,8 +1,8 @@
 """
 Tests for the function get_bag_size(), which gets the size of a bag from the Payload-Oxum in GB.
 
-Note: we did not want to store a large enough bag to not be zero,
-so use the template to test a bag on your local machine, if needed.
+Note: other than the bag with size zero, these bags are not valid.
+We updated the Payload-Oxum in bag-info.txt to test larger sizes without needing large bags in the repo.
 """
 import os
 import unittest
@@ -11,24 +11,32 @@ from validate_fixity import get_bag_size
 
 class MyTestCase(unittest.TestCase):
 
-    def test_small_bag(self):
-        """Test for a bag small enough to fit in the GitHub repo, which rounds to zero"""
+    def test_zero(self):
+        """Test for a bag small enough to round to zero"""
         # Makes the variable needed for function input and runs the function.
-        bag_path = os.path.join('test_data', 'get_bag_size', 'id_bag')
+        bag_path = os.path.join('test_data', 'get_bag_size', 'zero_bag')
         bag_size = get_bag_size(bag_path)
 
         # Verifies the function returned the correct value for bag_size.
-        self.assertEqual(0.0, bag_size, "Problem with test for small bag")
+        self.assertEqual(0.0, bag_size, "Problem with test for zero")
 
-    # def test_template(self):
-    #     """Test for a bag on a local computer, to be big enough to not be 0.0"""
-    #     # Makes the variable needed for function input and runs the function.
-    #     bag_path = 'INSERT PATH HERE'
-    #     bag_size = get_bag_size(bag_path)
-    #
-    #     # Verifies the function returned the correct value for bag_size.
-    #     expected = 'INSERT SIZE HERE'
-    #     self.assertEqual(expected, bag_size, "Problem with test for local bag")
+    def test_fraction(self):
+        """Test for a bag that is less than a GB"""
+        # Makes the variable needed for function input and runs the function.
+        bag_path = os.path.join('test_data', 'get_bag_size', 'fraction_bag')
+        bag_size = get_bag_size(bag_path)
+
+        # Verifies the function returned the correct value for bag_size.
+        self.assertEqual(0.1, bag_size, "Problem with test for fraction")
+
+    def test_gb(self):
+        """Test for a bag that is more than a GB"""
+        # Makes the variable needed for function input and runs the function.
+        bag_path = os.path.join('test_data', 'get_bag_size', 'gb_bag')
+        bag_size = get_bag_size(bag_path)
+
+        # Verifies the function returned the correct value for bag_size.
+        self.assertEqual(1.4, bag_size, "Problem with test for gb")
 
 
 if __name__ == '__main__':
