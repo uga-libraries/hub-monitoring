126fbbeb3427e8d6faf4a7ffb1d773141613e5fa
Adriane M Hanson <ahansonuga@gmail.com>
2025-11-26 16:46:21 -0500

    Move test data into test_data folder

--- tests/accession_completeness_report/test_check_completeness.py
+++ tests/accession_completeness_report/test_check_completeness.py
@@ -10,21 +10,21 @@ class MyTestCase(unittest.TestCase):
 
     def test_complete(self):
         """Test for an accession that is complete."""
-        completeness_dict = check_completeness(join('check_completeness_test_data', 'coll_1', 'acc_1_1'))
+        completeness_dict = check_completeness(join('test_data', 'check_completeness_test', 'coll_1', 'acc_1_1'))
         expected = {'pres_log': True, 'pres_log_format': None, 'full_risk': True,
                     'initial_manifest': True, 'bag': True}
         self.assertEqual(expected, completeness_dict, "Problem with test for complete")
 
     def test_not_complete(self):
         """Test for an accession that is missing all four required components."""
-        completeness_dict = check_completeness(join('check_completeness_test_data', 'coll_1', 'acc_1_2'))
+        completeness_dict = check_completeness(join('test_data', 'check_completeness_test', 'coll_1', 'acc_1_2'))
         expected = {'pres_log': False, 'pres_log_format': None, 'full_risk': False,
                     'initial_manifest': False, 'bag': False}
         self.assertEqual(expected, completeness_dict, "Problem with test for not complete")
 
     def test_pres_log_formatting(self):
         """Test for an accession with all four required components but the preservation log is formatted wrong."""
-        completeness_dict = check_completeness(join('check_completeness_test_data', 'coll_1', 'acc_1_3'))
+        completeness_dict = check_completeness(join('test_data', 'check_completeness_test', 'coll_1', 'acc_1_3'))
         expected = {'pres_log': False, 'pres_log_format': 'Nonstandard columns', 'full_risk': True,
                     'initial_manifest': True, 'bag': True}
         self.assertEqual(expected, completeness_dict, "Problem with test for pres log formatting")
