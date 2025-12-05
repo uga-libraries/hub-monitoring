5061d1e8a0f6ce8fd6ecd09f1cc08c61fdaf9e4f
Adriane M Hanson <ahansonuga@gmail.com>
2025-12-05 13:53:45 -0500

    Split unit tests for end, equal, or start with

--- tests/accession_completeness_report/test_accession_paths.py
+++ tests/accession_completeness_report/test_accession_paths.py
@@ -30,13 +30,26 @@ class MyTestCase(unittest.TestCase):
         expected = [join(coll_path, 'acc_3_1')]
         self.assertEqual(expected, accession_list, "Problem with test for skip file")
 
-    def test_skip_folders(self):
-        """Test for when each of the skipped folder names are present in the collection folder,
-        which should not be included, as well as two accession folders"""
+    def test_skip_endswith(self):
+        """Test for when accession_FITS that should not be included is present"""
         coll_path = join('accession_paths_test_data', 'coll_4')
         accession_list = accession_paths(coll_path)
         expected = [join(coll_path, 'acc_4_1'), join(coll_path, 'acc_4_2')]
-        self.assertEqual(expected, accession_list, "Problem with test for skip folders")
+        self.assertEqual(expected, accession_list, "Problem with test for skip endswith")
+
+    def test_skip_equal(self):
+        """Test for when each of the folder names that should not be included is present"""
+        coll_path = join('accession_paths_test_data', 'coll_5')
+        accession_list = accession_paths(coll_path)
+        expected = [join(coll_path, 'acc_5_1')]
+        self.assertEqual(expected, accession_list, "Problem with test for skip equal")
+
+    def test_skip_startswith(self):
+        """Test for when each of the prefixes for folder names that should not be included is present"""
+        coll_path = join('accession_paths_test_data', 'coll_6')
+        accession_list = accession_paths(coll_path)
+        expected = [join(coll_path, 'acc_6_1')]
+        self.assertEqual(expected, accession_list, "Problem with test for skip startswith")
 
 
 if __name__ == '__main__':
