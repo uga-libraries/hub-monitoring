4e2a4f62c1d3a18db305b0f7d6ff26addcafd0f9
Adriane M Hanson <ahansonuga@gmail.com>
2025-12-05 14:07:06 -0500

    Import entire library

--- tests/accession_completeness_report/test_check_completeness.py
+++ tests/accession_completeness_report/test_check_completeness.py
@@ -1,37 +1,37 @@
 """
 Tests for the function check_completeness(), which looks for all four things that should be in an accession folder.
 """
+import os
 import unittest
 from accession_completeness_report import check_completeness
-from os.path import join
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_complete(self):
         """Test for an accession that is complete."""
-        completeness_dict = check_completeness(join('test_data', 'check_completeness', 'coll_1', 'acc_1_1'))
+        completeness_dict = check_completeness(os.path.join('test_data', 'check_completeness', 'coll_1', 'acc_1_1'))
         expected = {'pres_log': True, 'pres_log_format': None, 'full_risk': True,
                     'initial_manifest': True, 'bag': True}
         self.assertEqual(expected, completeness_dict, "Problem with test for complete")
 
     def test_complete_bags(self):
         """Test for an accession that is complete and is split in multiple bags."""
-        completeness_dict = check_completeness(join('test_data', 'check_completeness', 'coll_1', 'acc_1_1'))
+        completeness_dict = check_completeness(os.path.join('test_data', 'check_completeness', 'coll_1', 'acc_1_1'))
         expected = {'pres_log': True, 'pres_log_format': None, 'full_risk': True,
                     'initial_manifest': True, 'bag': True}
         self.assertEqual(expected, completeness_dict, "Problem with test for complete")
 
     def test_not_complete(self):
         """Test for an accession that is missing all four required components."""
-        completeness_dict = check_completeness(join('test_data', 'check_completeness', 'coll_1', 'acc_1_2'))
+        completeness_dict = check_completeness(os.path.join('test_data', 'check_completeness', 'coll_1', 'acc_1_2'))
         expected = {'pres_log': False, 'pres_log_format': None, 'full_risk': False,
                     'initial_manifest': False, 'bag': False}
         self.assertEqual(expected, completeness_dict, "Problem with test for not complete")
 
     def test_pres_log_formatting(self):
         """Test for an accession with all four required components but the preservation log is formatted wrong."""
-        completeness_dict = check_completeness(join('test_data', 'check_completeness', 'coll_1', 'acc_1_3'))
+        completeness_dict = check_completeness(os.path.join('test_data', 'check_completeness', 'coll_1', 'acc_1_3'))
         expected = {'pres_log': False, 'pres_log_format': 'Nonstandard columns', 'full_risk': True,
                     'initial_manifest': True, 'bag': True}
         self.assertEqual(expected, completeness_dict, "Problem with test for pres log formatting")
--- tests/accession_completeness_report/test_script_accession_completeness_report.py
+++ tests/accession_completeness_report/test_script_accession_completeness_report.py
@@ -2,19 +2,18 @@
 Tests for the script accession_completeness_report.py, which tests each accession for if it contains the four
 required elements: preservation log, full risk report, initial manifest, and a bag.
 """
+from datetime import date
+import os
+import pandas as pd
 import subprocess
 import unittest
-from datetime import date
-from os import getcwd, remove
-from os.path import exists, join
-from pandas import read_csv
 
 
 def csv_to_list(csv_path):
     """Read csv into a dataframe, clean up, and return the values of each row as a list
     Blanks are filled with a string because np.nan comparisons work inconsistently.
     """
-    df = read_csv(csv_path)
+    df = pd.read_csv(csv_path)
     df = df.fillna('nan')
     csv_list = [df.columns.tolist()] + df.values.tolist()
     return csv_list
@@ -25,28 +24,28 @@ class MyTestCase(unittest.TestCase):
     def tearDown(self):
         """Deletes the test output if it was created"""
         today = date.today().strftime('%Y-%m-%d')
-        if exists(join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv')):
-            remove(join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv'))
+        if os.path.exists(os.path.join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv')):
+            os.remove(os.path.join('script_test_data', 'incomplete', f'accession_completeness_report_{today}.csv'))
 
     def test_complete(self):
         """Test for an input directory that contains only complete accessions, so no report is generated,
         as well as a folder at the status level that is not backlogged or closed and is skipped"""
         # Makes the variable used for script input and runs the script.
-        script = join(getcwd(), '..', '..', 'accession_completeness_report.py')
-        input_directory = join('script_test_data', 'complete')
+        script = os.path.join(os.getcwd(), '..', '..', 'accession_completeness_report.py')
+        input_directory = os.path.join('script_test_data', 'complete')
         script_message = subprocess.run(f'python {script} {input_directory}', shell=True, stdout=subprocess.PIPE)
 
         # Tests the correct message is printed.
         today = date.today().strftime('%Y-%m-%d')
         result = script_message.stdout.decode('utf-8')
-        expected = (f"Starting on accession {join(input_directory, 'backlogged', 'coll_1', 'acc_1_1')}\r\n"
-                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_1')}\r\n"
-                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_2')}\r\n"
+        expected = (f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_1', 'acc_1_1')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_2', 'acc_2_1')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_2', 'acc_2_2')}\r\n"
                     f"\r\nAll accessions are complete.\r\n")
         self.assertEqual(expected, result, "Problem with test for complete, printed message")
 
         # Tests the csv was not made.
-        result = exists(join(input_directory, f'accession_completeness_report_{today}.csv'))
+        result = os.path.exists(os.path.join(input_directory, f'accession_completeness_report_{today}.csv'))
         self.assertEqual(result, False, "Problem with test for complete, report")
 
     def test_incomplete(self):
@@ -54,19 +53,19 @@ class MyTestCase(unittest.TestCase):
         but also includes complete accessions and a collection from the unconventional collection list
         that will not be in the report"""
         # Makes the variable used for script input and runs the script.
-        script = join(getcwd(), '..', '..', 'accession_completeness_report.py')
-        input_directory = join('script_test_data', 'incomplete')
+        script = os.path.join(os.getcwd(), '..', '..', 'accession_completeness_report.py')
+        input_directory = os.path.join('script_test_data', 'incomplete')
         script_message = subprocess.run(f'python {script} {input_directory}', shell=True, stdout=subprocess.PIPE)
 
         # Tests the correct message is printed.
         today = date.today().strftime('%Y-%m-%d')
-        report_path = join(input_directory, f'accession_completeness_report_{today}.csv')
+        report_path = os.path.join(input_directory, f'accession_completeness_report_{today}.csv')
         result = script_message.stdout.decode('utf-8')
-        expected = (f"Starting on accession {join(input_directory, 'backlogged', 'coll_1', 'acc_1_1')}\r\n"
-                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_1')}\r\n"
-                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_2')}\r\n"
-                    f"Starting on accession {join(input_directory, 'backlogged', 'coll_2', 'acc_2_3')}\r\n"
-                    f"Starting on accession {join(input_directory, 'closed', 'coll_3', 'acc_3_1')}\r\n"
+        expected = (f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_1', 'acc_1_1')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_2', 'acc_2_1')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_2', 'acc_2_2')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'backlogged', 'coll_2', 'acc_2_3')}\r\n"
+                    f"Starting on accession {os.path.join(input_directory, 'closed', 'coll_3', 'acc_3_1')}\r\n"
                     f"\r\nIncomplete accessions found. See {report_path}.\r\n")
         self.assertEqual(expected, result, "Problem with test for incomplete, printed message")
 
